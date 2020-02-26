24c1fa1fc25b3dfe17f261c9367e3e718fee2da9
kasamatsu <kasamatsu@kasa.com>
2020-02-26 13:52:10 +0900

    最初のコミット

--- FillingLyricToMIDI/MainWindow.xaml.cs
+++ FillingLyricToMIDI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MIDIDataCSWrapper;
+using System.IO;
 
 namespace FillingLyricToMIDI
 {
@@ -20,9 +23,90 @@ namespace FillingLyricToMIDI
     /// </summary>
     public partial class MainWindow : Window
     {
+        MIDIData MidiData { get; set; } = null;
+
+        /// <summary>
+        /// midiファイル名を取得、設定します。
+        /// </summary>
+        string MidiFileName
+        {
+            get
+            {
+                return MidiFileNameTextBox.Text;
+            }
+            set
+            {
+                MidiFileNameTextBox.Text = value;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void FileSelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "MIDIファイル|*.mid|全てのファイル|*.*"
+            };
+            bool dialogResult = fileDialog.ShowDialog(this) ?? false; //戻り値がnullの場合、falseを代入
+            if (dialogResult)
+            {
+                MidiFileName = fileDialog.FileName;
+            } 
+        }
+
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(MidiFileName))
+            {
+                return;
+            }
+
+            MidiData = new MIDIData(MidiFileName);
+            MIDITrack track = null;
+            switch (MidiData.Format)
+            {
+                case MIDIData.Formats.Format0:
+                    track = MidiData[0];
+                    break;
+                case MIDIData.Formats.Format1:
+                    track = MidiData[1];
+                    break;
+                case MIDIData.Formats.Format2:
+                    return;
+                default:
+                    return;
+            }
+            //結合できるイベントを結合
+            foreach (MIDIEvent @event in track)
+            {
+                if (@event.IsNoteOn)            //ノートONで
+                {
+                    if (!@event.IsCombined)     //結合されていない場合
+                    {
+                        @event.Combine();
+                    }
+                }
+            }
+
+
+            SaveButton.IsEnabled = true;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog
+            {
+                Filter = "MIDIファイル|*.mid"
+            };
+            bool dialogResult = fileDialog.ShowDialog(this) ?? false; //戻り値がnullの場合、falseを代入
+            if (dialogResult)
+            {
+                MidiData?.SaveAsSMF(fileDialog.FileName);   //nullなら実行されない
+            }
         }
     }
 }
